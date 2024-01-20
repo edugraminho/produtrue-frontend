86c068d64fa91ce2c075dd72c5af81d92ee65975
Eduardo Graminho <edugraminho@gmail.com>
2024-01-20 13:18:18 -0300

    initial commit

---
+++ src/components/ProductListing/index.jsx
@@ -0,0 +1,12 @@
+export default function ProductListing({company, name, openingAmount}){
+
+  return (
+    <menu>
+      <h2>{company}</h2>
+      <p>{name}</p>
+      <p>{openingAmount}</p>
+
+    </menu>
+  )
+
+} 
